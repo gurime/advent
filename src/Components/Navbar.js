30e55f055695aca70155d405d3bcb6224e781aca
Phillip Bailey <ztron500@gmail.com>
2023-04-25 16:05:43 -0400

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import adventpic from "./img/journey-logo.png"
+// import adventpic from "./img/journey-logo.png"
 import { useRouter } from 'next/router'
 const Navbar = () => {
 const router = useRouter()    
 return (
 <>
-<header>
+<header className='header'>
 <div className="header-logo-box">
-
-<Image width={100}
+Logo
+{/* <Image width={100}
 onClick={() => router.push('/')}
-src={adventpic} alt="..." priority/>
+src={adventpic} alt="..." priority/> */}
 </div>  
 
-<div class="contact-box">
+<div className="contact-box">
 
-<div>
-<Link style={{textDecoration:'none'}}  
-href="#!" className="contact-open">Call us, we're open 24/7</Link>
+<div style={{padding:'1rem'}}>
+Call us, we're open 24/7
 </div>
 
-<div>
-<Link style={{textDecoration:'none'}} href="#!">1-800-234-5678</Link>
+<div style={{padding:'1rem'}}>
+1-800-234-5678
 </div>
 
 </div>
